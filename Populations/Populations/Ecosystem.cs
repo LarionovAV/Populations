47199447d8930b67f055c4972e4c07504b2b69a2
Larionov Alexey <larionov.develop@yandex.ru>
2020-01-13 17:40:16 +0700

    Классы

---
+++ Populations/Populations/Ecosystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Populations
+{
+    class Ecosystem
+    {
+        private List<Species> Species { get; set; }
+        private double[,] Relations { get; set; }
+        private Ecosystem instance = null;
+
+        private Ecosystem(){
+            Init();
+        }
+    }
+}
