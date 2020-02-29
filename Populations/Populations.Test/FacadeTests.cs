bc4666a58e420ea39e1ce4203b20f466943d0ff5
Larionov Alexey <larionov.develop@yandex.ru>
2020-02-29 14:24:16 +0700

    Revert "Revert "Tests""
    This reverts commit 04c85c2caecfea48e30cde2ebaa1053f11bb94f3.

--- Populations/Populations.Test/FacadeTests.cs
+++ Populations/Populations.Test/FacadeTests.cs
@@ -10,21 +10,44 @@ using System.Threading.Tasks;
 namespace Populations.Test
 {
     [TestClass]
-    public class SnapshotTests
+    public class FacadeTests
     {
         [TestMethod]
-        public void SnapshotTest() {
+        public void ExecuteTest() {
+            //Init data
+            int currentPopulation = 10;
+            int birth = 3;
+            int death = 1;
+            int ecoCapacity = 200;
+            int expectedpopulation = 29;
+
+            //Actions
+            Species fox = new Species("fox", currentPopulation, birth, death, ecoCapacity);
+            Ecosystem.GetInstance().AddSpecies(fox);
+            LogisticModel lm = new LogisticModel();
+            Facade.Execute();
+
+            //Check
+            Assert.AreEqual(expectedpopulation, fox.Population);
+
+            Ecosystem.GetInstance().Species.Clear();
+            History.GetInstance().Data.Clear();
+        }
+
+        [TestMethod]
+        public void TakeSnapshotTest()
+        {
             int startPopulation = 10;
             Species fox = new Species("fox", startPopulation, 3, 1, 200);
             Ecosystem.GetInstance().AddSpecies(fox);
-            Memento.Save();
+            Facade.TakeSnapshot();
 
             IMathModel model = new LogisticModel();
             model.Recount();
 
             Assert.AreEqual(29, fox.Population);
 
-            Memento.Restore();
+            Facade.Restore();
             Assert.AreEqual(startPopulation, Ecosystem.GetInstance().Species[0].Population);
 
             Ecosystem.GetInstance().Species.Clear();
